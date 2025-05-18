9faf120856c6964fe2e2446247f7a4c46ee17a8f
Wiggidy <rlr0017@auburn.edu>
2025-05-18 18:37:41 -0500

    Update to use the new monolithic ObjectiveAI API

--- protoc.ps1
+++
@@ -1,6 +0,0 @@
-npx protoc `
-    --ts_out src/query/proto/ `
-    --ts_opt "ts_nocheck" `
-    --ts_opt "server_none" `
-    --ts_opt "long_type_number" `
-    objective_ai_proto/objective_ai.proto
